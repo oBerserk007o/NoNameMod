b84d7a6d24f49c627231e2b65fca8dbb0dbb0fba
oBerserk007o <89747160+oBerserk007o@users.noreply.github.com>
2022-08-05 12:48:42 -0400

    Addded Flee Stick and tweaked some other files

--- src/main/java/com/Berserk007/nonamemod/item/ModItems.java
+++ src/main/java/com/Berserk007/nonamemod/item/ModItems.java
@@ -2,6 +2,7 @@ package com.Berserk007.nonamemod.item;
 
 import com.Berserk007.nonamemod.NoNameMod;
 import com.Berserk007.nonamemod.item.custom.FireballStaffItem;
+import com.Berserk007.nonamemod.item.custom.FleeStickItem;
 import com.Berserk007.nonamemod.item.custom.LightningStaffItem;
 import com.Berserk007.nonamemod.item.custom.UpgradedCoalItem;
 import net.minecraft.world.item.*;
@@ -45,28 +46,42 @@ public class ModItems {
     public static final RegistryObject<Item> LIGHTNING_STAFF = ITEMS.register("lightning_staff",
             () -> new LightningStaffItem(new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).durability(100)));
 
+    public static final RegistryObject<Item> FLEE_STICK = ITEMS.register("flee_stick",
+            () -> new FleeStickItem(ModTiers.GENERIC, 1, -3f,
+                    new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB)));
+
 
     public static final RegistryObject<Item> PYRO_CONDENSATE_SWORD = ITEMS.register("pyro_condensate_sword",
-            () -> new SwordItem(ModTiers.PYRO, 3, -2.4f, new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
+            () -> new SwordItem(ModTiers.PYRO, 3, -2.4f,
+                    new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
     public static final RegistryObject<Item> PYRO_CONDENSATE_AXE = ITEMS.register("pyro_condensate_axe",
-            () -> new AxeItem(ModTiers.PYRO, 5f, -3f, new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
+            () -> new AxeItem(ModTiers.PYRO, 5, -3f,
+                    new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
     public static final RegistryObject<Item> PYRO_CONDENSATE_PICKAXE = ITEMS.register("pyro_condensate_pickaxe",
-            () -> new PickaxeItem(ModTiers.PYRO, 1, -2.8f, new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
+            () -> new PickaxeItem(ModTiers.PYRO, 1, -2.8f,
+                    new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
     public static final RegistryObject<Item> PYRO_CONDENSATE_HOE = ITEMS.register("pyro_condensate_hoe",
-            () -> new HoeItem(ModTiers.PYRO, -4, 0f, new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
+            () -> new HoeItem(ModTiers.PYRO, -4, 0f,
+                    new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
     public static final RegistryObject<Item> PYRO_CONDENSATE_SHOVEL = ITEMS.register("pyro_condensate_shovel",
-            () -> new ShovelItem(ModTiers.PYRO, 3, -2.4f, new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
+            () -> new ShovelItem(ModTiers.PYRO, 3, -2.4f,
+                    new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
 
     public static final RegistryObject<Item> ELECTRO_CONDENSATE_SWORD = ITEMS.register("electro_condensate_sword",
-            () -> new SwordItem(ModTiers.ELECTRO, 3, -2.4f, new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
+            () -> new SwordItem(ModTiers.ELECTRO, 3, -2.4f,
+                    new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
     public static final RegistryObject<Item> ELECTRO_CONDENSATE_AXE = ITEMS.register("electro_condensate_axe",
-            () -> new AxeItem(ModTiers.ELECTRO, 5f, -3f, new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
+            () -> new AxeItem(ModTiers.ELECTRO, 5, -3f,
+                    new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
     public static final RegistryObject<Item> ELECTRO_CONDENSATE_PICKAXE = ITEMS.register("electro_condensate_pickaxe",
-            () -> new PickaxeItem(ModTiers.ELECTRO, 1, -2.8f, new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
+            () -> new PickaxeItem(ModTiers.ELECTRO, 1, -2.8f,
+                    new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
     public static final RegistryObject<Item> ELECTRO_CONDENSATE_HOE = ITEMS.register("electro_condensate_hoe",
-            () -> new HoeItem(ModTiers.ELECTRO, -4, 0f, new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
+            () -> new HoeItem(ModTiers.ELECTRO, -4, 0f,
+                    new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
     public static final RegistryObject<Item> ELECTRO_CONDENSATE_SHOVEL = ITEMS.register("electro_condensate_shovel",
-            () -> new ShovelItem(ModTiers.ELECTRO, 3, -2.4f, new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
+            () -> new ShovelItem(ModTiers.ELECTRO, 3, -2.4f,
+                    new Item.Properties().tab(ModCreativeModeTab.NONAMEMOD_TAB).fireResistant()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/com/Berserk007/nonamemod/item/custom/FleeStickItem.java
@@ -0,0 +1,44 @@
+package com.Berserk007.nonamemod.item.custom;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class FleeStickItem extends SwordItem {
+    public FleeStickItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
+        super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> tooltipComponents, @NotNull TooltipFlag isAdvanced) {
+        if(Screen.hasShiftDown()){
+            tooltipComponents.add(new TranslatableComponent("tooltip.nonamemod.flee_stick"));
+        }
+        else {
+            tooltipComponents.add(new TranslatableComponent("tooltip.nonamemod.shift"));
+        }
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        target.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 200), attacker);
+        target.addEffect(new MobEffectInstance(MobEffects.GLOWING, 200), attacker);
+        target.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 200), attacker);
+        target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 200), attacker);
+        attacker.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), attacker);
+        attacker.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY, 20), attacker);
+        return super.hurtEnemy(stack, target, attacker);
+    }
+}
